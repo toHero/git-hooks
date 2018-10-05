e8dd895268d769f702cb5462312b35208338b2ea
Antoine Chabert <antoine.chabert@tohero.fr>
2018-10-05 16:51:49 +0200

    Use array of hook name for make it generic in scripts
    Signed-off-by: Antoine Chabert <antoine.chabert@tohero.fr>

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,22 @@
 
 source $(dirname $0)/config.sh
 
-sudo mkdir -p ${TOHERO_DIR} && \
-sudo chown ${USER}:${USER} ${TOHERO_DIR} && \
-git clone ${REPOSITORY} ${TOHERO_HOOKS_DIR} && \
-cd ${GIT_HOOKS_TEMPLATE_DIR} && \
-sudo ln -s ${TOHERO_HOOKS_DIR}/commit-msg && \
-sudo ln -s ${TOHERO_HOOKS_DIR}/pre-commit && \
+# Create directory if not exist
+sudo mkdir -p ${TOHERO_DIR} || exit 1
+
+# Set right owner if not the case
+sudo chown ${USER}:${USER} ${TOHERO_DIR} || exit 1
+
+# Clone repository
+git clone ${REPOSITORY} ${TOHERO_HOOKS_DIR} || exit 1
+
+# Go inside the git template directory to add links to toHero hooks
+cd ${GIT_HOOKS_TEMPLATE_DIR} || exit 1
+
+# Add links
+for hook in "${HOOKS[@]}"
+do
+    sudo ln -s "${TOHERO_HOOKS_DIR}/${hook}" || echo "unable to add '${TOHERO_HOOKS_DIR}/${hook}' hook link in '${GIT_HOOKS_TEMPLATE_DIR}'"
+done
+
 echo "Installation succeed"
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,7 +2,13 @@
 
 source $(dirname $0)/config.sh
 
-sudo unlink ${GIT_HOOKS_TEMPLATE_DIR}/commit-msg 2>/dev/null >&2 ; \
-sudo unlink ${GIT_HOOKS_TEMPLATE_DIR}/pre-commit 2>/dev/null >&2 ; \
-sudo rm -rf ${TOHERO_HOOKS_DIR} 2>/dev/null >&2 ; \
+# Remove hooks
+for hook in "${HOOKS[@]}"
+do
+    sudo unlink "${GIT_HOOKS_TEMPLATE_DIR}/${hook}" 2>/dev/null >&2
+done
+
+# Remove git hook repository
+sudo rm -rf ${TOHERO_HOOKS_DIR} 2>/dev/null >&2
+
 echo "Uninstall succeed"
